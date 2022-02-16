fd90c9a95bd999635a408d1200b223b8cfc02a68
Danil Ochagov <danilochagov@gmail.com>
2022-02-16 13:16:25 +0300

    Initial commit

---
+++ Kursovaya_Console/Kursovaya_Console/Kursovaya_Console.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <clocale>
+#include <windows.h>
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    // Add Russian language support
+    setlocale(LC_CTYPE, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Console::BufferHeight = Console::WindowHeight;
+    Console::BufferWidth = Console::WindowWidth;
+    Console::CursorVisible::set(false);
+    Console::ForegroundColor = ConsoleColor::Yellow;
+    Console::BackgroundColor = ConsoleColor::Cyan;
+
+    printf("Hello, World!");
+    
+    _getch();
+    return 0;
+}
