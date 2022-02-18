e34efdde84ba6a684bab7a84b1805577af59cbfc
Danil Ochagov <danilochagov@gmail.com>
2022-02-18 13:21:24 +0300

    Add menu items

--- Kursovaya_Console/Kursovaya_Console/Kursovaya_Console.cpp
+++ Kursovaya_Console/Kursovaya_Console/Kursovaya_Console.cpp
@@ -23,30 +23,28 @@ struct z {
     int failureFlights;
 };
 
-// int menu(int);
-// void maxim(struct z*);
-// void first(struct z*);
-// void text_data(char*, char*);
-// void kolvo(struct z*);
-// void alfalist(struct z*);
-// void vstavka(struct z*, char*);
-// void listing(struct z*);
-// void diagram(struct z*);
+char dan[7][70] = {
+       "Среднее арифметическое успешных запусков?                      ",
+       "Какая самая дешевая ракета для запуска?                        ",
+       "Список ракет, у которых число удачных запусков больше X        ",
+       "Список ракет, запуск которых стоит больше XM$ и меньше XM$     ",
+       "Алфавитный список всех компаний                                ",
+       "Диаграмма. Процентное соотношение всех запусков каждой компании",
+       "Выход                                                          "
+};
+
+int menu(int);
+void avrSuccessfulFlights(struct z*);
+void cheapestRocket(struct z*);
+void successfulFlightsBiggerThan(struct z*);
+void costPerLaunchBiggerAndLessThan(struct z*);
+void alphabet(struct z*);
+void diagram(struct z*);
 
 int main(array<System::String ^> ^args)
 {
     // Some variables
-    char dan[7][70] = {
-        "Какая компания имеет больше всего неудачных запусков?",
-        "Какая самая дешевая ракета для запуска?",
-        "Список ракет, у которых число удачных запусков больше X",
-        "Список ракет, запуск которых стоит меньше XM$",
-        "Алфавитный список всех компаний",
-        "Диаграмма. Процентное соотношение всех запусков каждой компании",
-        "Выход"
-    };
-
-    char BlankLine[80]; memset(BlankLine, ' ', 56);
+    char BlankLine[80]; memset(BlankLine, ' ', 65); BlankLine[65] = 0;
     char filePath[90] = "D:\\University\\прога\\курсач\\Kursovaya_Console\\Kursovaya_Console\\SpaceCompanies.dat";
     int NC, i, n;
     FILE* in;
@@ -94,7 +92,115 @@ int main(array<System::String ^> ^args)
             companies[i].pricePerLaunch,
             companies[i].successfulFlights,
             companies[i].failureFlights);
-    
+
     _getch();
+
+    // Dispaly menu
+    while (1)
+    {
+        Console::ForegroundColor = ConsoleColor::Red;
+        Console::BackgroundColor = ConsoleColor::Blue;
+        Console::Clear();
+        Console::ForegroundColor = ConsoleColor::Yellow;
+        Console::BackgroundColor = ConsoleColor::Red;
+        Console::CursorLeft = 25;
+        Console::CursorTop = 4;
+        printf(BlankLine);
+
+        for (i = 0; i < 7; i++)
+        {
+            Console::CursorLeft = 25;
+            Console::CursorTop = i + 5;
+            printf(" %s ", dan[i]);
+        }
+
+        Console::CursorLeft = 25;
+        Console::CursorTop = 12;
+        printf(BlankLine);
+
+        n = menu(7); // Select current item in menu
+        switch (n)
+        {
+            case 1: avrSuccessfulFlights(companies); break;
+            case 2: cheapestRocket(companies); break;
+            case 3: successfulFlightsBiggerThan(companies); break;
+            case 4: costPerLaunchBiggerAndLessThan(companies); break;
+            case 5: alphabet(companies); break;
+            case 6: diagram(companies); break;
+            case 7: exit(0);
+        }
+    }
+
     return 0;
+}
+
+int menu(int n)
+{
+    int y1 = 0, y2 = n - 1;
+    char c = 1;
+    while (c != ESC)
+    {
+        switch (c)
+        {
+            case DOWN:y2 = y1; y1++; break;
+            case UP: y2 = y1; y1--; break;
+            case ENTER: return y1 + 1;
+        }
+
+        if (y1 > n - 1)
+        {
+            y2 = n - 1; 
+            y1 = 0;
+        }
+        if (y1 < 0)
+        {
+            y2 = 0;
+            y1 = n - 1;
+        }
+
+        Console::ForegroundColor = ConsoleColor::White;
+        Console::BackgroundColor = ConsoleColor::Green;
+        Console::CursorLeft = 25;
+        Console::CursorTop = y1 + 5;
+        printf(" %s ", dan[y1]);
+
+        Console::ForegroundColor = ConsoleColor::Yellow;
+        Console::BackgroundColor = ConsoleColor::Red;
+        Console::CursorLeft = 25;
+        Console::CursorTop = y2 + 5;
+        printf(" %s ", dan[y2]);
+        c = _getch();
+    }
+
+    exit(0);
+}
+
+void avrSuccessfulFlights(struct z* company)
+{
+
+}
+
+void cheapestRocket(struct z* company)
+{
+
+}
+
+void successfulFlightsBiggerThan(struct z* company)
+{
+
+}
+
+void costPerLaunchBiggerAndLessThan(struct z* company)
+{
+
+}
+
+void alphabet(struct z* company)
+{
+
+}
+
+void diagram(struct z* company)
+{
+
 }
